3d28355942c829a21ae0e07236f336c474911084
Russell Steicke <russells@adelie.cx>
2012-02-23 23:24:11 +0800

    Select the pins to use for the LEDs.
    Signed-off-by: Russell Steicke <russells@adelie.cx>

--- flashy3.c
+++ flashy3.c
@@ -1,8 +1,41 @@
 
 
 #include <avr/io.h>
+#include "flashy3.h"
+
+
+static void setup_ports(void);
+
 
 int main(void)
 {
+	setup_ports();
+}
 
+
+static void setup_ports(void)
+{
+	LED1DDR |= (1 << LED1R);
+	LED1DDR |= (1 << LED1G);
+	LED1DDR |= (1 << LED1B);
+
+	LED2DDR |= (1 << LED2R);
+	LED2DDR |= (1 << LED2G);
+	LED2DDR |= (1 << LED2B);
+
+	LED3DDR |= (1 << LED3R);
+	LED3DDR |= (1 << LED3G);
+	LED3DDR |= (1 << LED3B);
+
+	LED1PORT &= ~ (1 << LED1R);
+	LED1PORT &= ~ (1 << LED1G);
+	LED1PORT &= ~ (1 << LED1B);
+
+	LED2PORT &= ~ (1 << LED2R);
+	LED2PORT &= ~ (1 << LED2G);
+	LED2PORT &= ~ (1 << LED2B);
+
+	LED3PORT &= ~ (1 << LED3R);
+	LED3PORT &= ~ (1 << LED3G);
+	LED3PORT &= ~ (1 << LED3B);
 }
---
+++ flashy3.h
@@ -0,0 +1,26 @@
+#ifndef flashy3_h_INCLUDED
+#define flashy3_h_INCLUDED
+
+#define LED1PORT PORTA
+#define LED1PIN  PINA
+#define LED1DDR  DDRA
+#define LED1R    6
+#define LED1G    5
+#define LED1B    4
+
+#define LED2PORT PORTA
+#define LED2PIN  PINA
+#define LED2DDR  DDRA
+#define LED2R    3
+#define LED2G    2
+#define LED2B    1
+
+#define LED3PORT PORTB
+#define LED3PIN  PINB
+#define LED3DDR  DDRB
+#define LED3R    0
+#define LED3G    1
+#define LED3B    2
+
+
+#endif
