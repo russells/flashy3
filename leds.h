7e15ea9b946d372af3232504d0fc781952c931b7
Russell Steicke <russells@adelie.cx>
2012-06-11 21:01:29 +0800

    Note pin assignments in the source.
    Signed-off-by: Russell Steicke <russells@adelie.cx>

--- leds.h
+++ leds.h
@@ -6,23 +6,26 @@
 #define LED1PORT PORTA
 #define LED1PIN  PINA
 #define LED1DDR  DDRA
-#define LED1R    6
-#define LED1G    5
-#define LED1B    4
+#define LED1R    6		/* Pin 19 */
+#define LED1G    5		/* Pin 18 */
+#define LED1B    4		/* Pin 17 */
 
 #define LED2PORT PORTA
 #define LED2PIN  PINA
 #define LED2DDR  DDRA
-#define LED2R    3
-#define LED2G    2
-#define LED2B    1
+#define LED2R    3		/* Pin 16 */
+#define LED2G    2		/* Pin 15 */
+#define LED2B    1		/* Pin 14 */
 
 #define LED3PORT PORTB
 #define LED3PIN  PINB
 #define LED3DDR  DDRB
-#define LED3R    1
-#define LED3G    2
-#define LED3B    3
+#define LED3R    1		/* Pin 2 */
+#define LED3G    2		/* Pin 3 */
+#define LED3B    3		/* Pin 4 */
+
+/* PB4 (DI, pin 5), PB5 (DO, pin 6), and PB6 (SCK, pin 7) are used for ICSP.
+   We could use them for LEDS, but it's simplest to leave them uncommitted. */
 
 
 struct FlashyLEDStatus {
